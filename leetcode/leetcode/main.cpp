1c832a0eea7702ccc8782a739bac8ad7231ebf2e
yuhang peng <2236326140@qq.com>
2022-07-26 15:00:03 +0800

    7.26

--- leetcode/leetcode/main.cpp
+++ leetcode/leetcode/main.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 using namespace std;
 #define ipt(x) cin>>x
+int add(int a, int b);
 int main() {
 	int a = 1;
 	int b = 1;
 	ipt(a);
 	cout << a << endl;
 	return 0;
+}
+int add(int a, int b) {
+	return a + b;
 }
